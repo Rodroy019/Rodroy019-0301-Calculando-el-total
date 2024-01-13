a9b091d7737aaed91c79e10779f7d24825df9041
Rodrigo Rivas <rodroy019@hotmail.com>
2024-01-13 00:02:36 -0300

    Change: se realizan cambios en codigo, todo codigo JavaScript se encuentra en .js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-precio = 400000;
-total = 0;
+let precio = 400000;
+let total = 0;
 
-precioSpan = document.querySelector(".precio-inicial");
+let precioSpan = document.getElementById("precioInicial");
 precioSpan.innerHTML = precio;
-totalSpan = document.querySelector(".valor-total");
+let totalSpan = document.getElementById("valorTotal");
 totalSpan.innerHTML = total;
+let cantidadActual = document.getElementById("cantidad");
+let btnMinus = document.getElementById("btnMinus");
+let btnPlus = document.getElementById("btnPlus");
+
+
+btnMinus.addEventListener("click", function(e){
+    let nuevaCantidad = Number(cantidadActual.innerHTML) -1;
+    if (nuevaCantidad < 0){nuevaCantidad = 0};
+    cantidadActual.innerHTML = nuevaCantidad;
+    totalSpan.innerHTML = nuevaCantidad * precio;
+});
+
+btnPlus.addEventListener("click", function(e){
+    let nuevaCantidad = Number(cantidadActual.innerHTML) +1;
+    cantidadActual.innerHTML = nuevaCantidad;
+    totalSpan.innerHTML = nuevaCantidad * precio;
+});
